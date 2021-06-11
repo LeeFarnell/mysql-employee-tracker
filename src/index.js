5a87741988883f40369dd307a856163b0acf7359
Suraj Verma <surajverma@rentalcars.com>
2021-06-11 16:06:22 +0100

    remove console logs for update and delete operations

--- src/index.js
+++ src/index.js
@@ -97,44 +97,35 @@ const init = async () => {
       } else if (action === "removeEmployee") {
         const { id } = await deleteEmployees(db);
         const query = 'DELETE FROM ?? WHERE ??="?"';
-        const data = await db.parameterisedQuery(query, ["employee", "id", id]);
-        console.table(data);
+        await db.parameterisedQuery(query, ["employee", "id", id]);
       } else if (action === "removeRole") {
         const { id } = await deleteRoles(db);
         const query = 'DELETE FROM ?? WHERE ??="?"';
-        const data = await db.parameterisedQuery(query, ["role", "id", id]);
-        console.table(data);
+        await db.parameterisedQuery(query, ["role", "id", id]);
       } else if (action === "removeDepartment") {
         const { id } = await deleteDepartments(db);
         const query = 'DELETE FROM ?? WHERE ??="?"';
-        const data = await db.parameterisedQuery(query, [
-          "department",
-          "id",
-          id,
-        ]);
-        console.table(data);
+        await db.parameterisedQuery(query, ["department", "id", id]);
       } else if (action === "updateEmployeeRole") {
         const { role, id } = await updateEmployeeRole(db);
         const query = "UPDATE ?? SET ??=? WHERE ??=?";
-        const data = await db.parameterisedQuery(query, [
+        await db.parameterisedQuery(query, [
           "employee",
           "role_id",
           role,
           "id",
           id,
         ]);
-        console.table(data);
       } else if (action === "updateEmployeeManager") {
         const { manager, id } = await updateEmployeeManager(db);
         const query = "UPDATE ?? SET ??=? WHERE ??=?";
-        const data = await db.parameterisedQuery(query, [
+        await db.parameterisedQuery(query, [
           "employee",
           "manager_id",
           manager,
           "id",
           id,
         ]);
-        console.table(data);
       } else if (action === "viewBudget") {
         const { id } = await totalBudget(db);
         const query = "SELECT SUM(??) FROM ?? WHERE ??=?";
